6233fdf3218fcce2033600e99a6dcd77d2b48e69
jaimezzapata <zapataval2304@gmail.com>
2026-03-17 08:54:12 -0500

    (chore): Configuración de los endpoints de acceso a la url de la API

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+const URL_BASE = `localhost:8080/`
+let end_points = {
+    users: URL_BASE + "users", /* "localhost:8080/usuarios" */
+    offers: URL_BASE + "offers",
+    candidates: URL_BASE + "candidates"
+}
